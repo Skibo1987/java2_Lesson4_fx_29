f0f07b9837476e8cbefd7c3fdc30e03460dd21be
geekbrains <info@geekbrains.ru>
2021-07-29 13:15:06 -0700

    Lesson4_HW

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Домашнее задание - Чат");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setScene(new Scene(root, 650, 550));
         primaryStage.show();
 
 
